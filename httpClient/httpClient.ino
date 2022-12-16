54cbe81c02af63976460a537e6be1a271a0ab3b4
Pavel Lyubaev <gabba1981@gmail.com>
2022-12-16 17:19:15 +0500

    working

--- httpClient/httpClient.ino
+++ httpClient/httpClient.ino
@@ -1,18 +1,24 @@
 #include <WiFi.h>
 #include "HTTPClient.h"
 #include "ArduinoJson.h"
+#include "DHT.h"
 
 #define LED 2
+#define DHTPIN 4
+#define DHTTYPE DHT22
 #define uS_TO_S_FACTOR 1000000
 #define mS_TO_S_FACTOR 1000
 #define SECONDS_TO_SLEEP 60
-#define DEEP_SLEEP false
+#define DEEP_SLEEP true
+
 
 // Replace with your network credentials (STATION)
-const char* ssid = "gabba";
-const char* password = "qwerty123456";
+const char* ssid = "iot-network";
+const char* password = "iot-pa$$w0rd";
 const String serverName = "https://iot.cloud.ga66a.ru";
 const String apiPath = "/api/v1/iot";
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void printWiFiStatus() {
   Serial.println("");
@@ -46,17 +52,22 @@ void serverExchange() {
 }
 
 void serverPost() {
-  String path = serverName + apiPath + "/device/";
-  // DynamicJsonBuffer jsonBuffer(1024);
-  // JsonObject& device = jsonBuffer.createObject();
-  // device["mak"] = WiFi.macAddress();
-  // JsonArray& indicators = device.createNestedArray("indicators");
+  // float humidity = dht.readHumidity();
+  // float temperature = dht.readTemperature();
 
-  // JsonObject& indicator_0 = indicators.createNestedObject();
-  // indicator_0["name"] = "LED";
-  // indicator_0["cuddentState"] = "0";
-  // indicator_0["type"] = "Switch";
-  StaticJsonDocument<200> device;
+
+  // char humidity[10];
+  // dtostrf(dht.readHumidity(), 5, 2, humidity);
+  // char temperature[10];
+  // dtostrf(dht.readTemperature(), 5, 2, temperature);
+
+
+  // Serial.println((String) "Temperature: " + temperature);
+  // Serial.println((String) "Humidity: " + humidity);
+
+  String path = serverName + apiPath + "/device/";
+
+  StaticJsonDocument<400> device;
   device["mak"] = WiFi.macAddress();
   JsonArray indicators = device.createNestedArray("indicators");
 
@@ -67,11 +78,19 @@ void serverPost() {
   indicator_0["currentState"] = digitalRead(LED);
   indicator_0["type"] = "Switch";
 
-  ///TEST_TEST
+  ///DHT Temperature
   JsonObject indicator_1 = indicators.createNestedObject();
-  indicator_1["name"] = "TEST SWITCH 2";
-  indicator_1["currentState"] = "0";
-  indicator_1["type"] = "Switch";
+  indicator_1["name"] = "DHT22T";
+  indicator_1["pin"] = DHTPIN;
+  indicator_1["indication"] = dht.readTemperature();
+  indicator_1["type"] = "Sensor";
+
+  ///DHT Temperature
+  JsonObject indicator_2 = indicators.createNestedObject();
+  indicator_2["name"] = "DHT22H";
+  indicator_2["pin"] = DHTPIN;
+  indicator_2["indication"] = dht.readHumidity();
+  indicator_2["type"] = "Sensor";
 
   String request;
   serializeJson(device, request);
@@ -123,6 +142,7 @@ void setup() {
   Serial.begin(115200);
   pinMode(LED, OUTPUT);
   initWiFi();
+  dht.begin();
 }
 
 void loop() {
@@ -137,7 +157,7 @@ void loop() {
     serverExchange();
   }
   //delay(10000);
-  Serial.println((String) "Going to sleep for "+SECONDS_TO_SLEEP+" seconds.");
+  Serial.println((String) "Going to sleep for " + SECONDS_TO_SLEEP + " seconds.");
   Serial.flush();
   delay(1000);
   if (DEEP_SLEEP) {
